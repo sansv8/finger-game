bd4b0c8841ea40bd9c2f08ec44da846fe42f2fd3
EthanTB <ethantberkley@gmail.com>
2023-03-30 18:21:42 -0500

    manual ip address config

--- client.py
+++ client.py
@@ -2,11 +2,12 @@
 
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
+host = input("enter host name: ")  # The server's hostname or IP address
+
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect((host, PORT))
     s.sendall(b"Hello, world")
     data = s.recv(1024)
 
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+host = input("enter host name: ")  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    s.bind((host, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
