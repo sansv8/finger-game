ea5e927947de7636ec9fa7185ef0e02816bbaafc
sansv8 <amith.p.pang@gmail.com>
2023-03-30 20:03:59 -0500

    Update server

--- server.py
+++ server.py
@@ -1,16 +1,38 @@
 import socket
-
+import player 
 host = input("enter host name: ")  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+# Print player hands
+def printPlayerHands(player1, player2):
+    print(f"Their hand:")
+    print(f"{player2.leftH.fingers}\t{player2.rightH.fingers}\n\n")
+    print(f"Your hand:")
+    print(f"{player1.leftH.fingers}\t{player1.rightH.fingers}")
+
+# Check win condition
+def playerLoss(player):
+    # If both fingers are 0
+    if(player.leftH.fingers == 0 and player.rightH.fingers == 0):
+        # Return true
+        return True
+    
+    # Otherwise, return false
+    return False
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((host, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
+        # Intiailize players
+        serverPlayer = player(True, True)
+        clinetPlayer = player(True, True)
+
+        # Print the player hands
+        printPlayerHands(serverPlayer, clinetPlayer)
+
+        # Loop until true
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+            break
+
